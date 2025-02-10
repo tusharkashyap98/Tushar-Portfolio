50752a19dc24542b245721bbbaa918a925509df1
Tushar Kashyap <tusharkashyap888@gamil.com>
2025-02-10 23:53:01 +0530

    built portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      transitionTimingFunction:{
+        'jump': 'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+      },
+    },
+    
+  },
+  plugins: [],
+};
